575bd7c4b5966993cd8166a51bd178134949d90c
Dan <dancoman@live.com>
2022-04-14 14:53:32 +0200

    jest config

--- src/tests/AppNavigator.test.jsx
+++ src/tests/AppNavigator.test.jsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+// import {NavigationContainer} from '@react-navigation/native';
 import {render, fireEvent} from '@testing-library/react-native';
 
-import AppNavigator from '../AppNavigator';
+// import AppNavigator from '@src/AppNavigator';
+import First from '@src/screens/first/FirstScreen';
 
 describe('Testing react navigation', () => {
   test('page contains the header and 10 items', async () => {
-    const component = (
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    );
+    const component = <First />;
 
     const {findByText} = render(component);
 
